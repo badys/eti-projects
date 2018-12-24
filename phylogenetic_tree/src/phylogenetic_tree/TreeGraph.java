964e596534b0bde4c361767b2494fd108c516b15
badys <piotr.badysiak@gmail.com>
2018-12-24 19:50:47 +0100

    expanding whitespaces to fit different decimal points in weights

--- phylogenetic_tree/src/phylogenetic_tree/TreeGraph.java
+++ phylogenetic_tree/src/phylogenetic_tree/TreeGraph.java
@@ -1,5 +1,6 @@
 package phylogenetic_tree;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -9,13 +10,18 @@ import java.util.List;
 public class TreeGraph {
 
     public static void showTreeGraph(List<TreeNode> treeList) {
-        main.clearConsole();
         System.out.println("Select tree to show a graph of");
-        for (int i = 1; i < treeList.size(); i++) {
-                System.out.println(String.format("%d. %s", i, treeList.get(i - 1).getRawFormat()));
-            }
+        for (int i = 1; i <= treeList.size(); i++) {
+            System.out.println(String.format("%d. %s", i, treeList.get(i - 1).getRawFormat()));
+        }
         int menuIndex = main.selectItemFromMenu();
-        print(treeList.get(menuIndex-1));
+        print(treeList.get(menuIndex - 1));
+        try {
+            System.out.print("Press enter key to leave preview.");
+            System.in.read();
+        } catch (IOException ex) {
+            System.out.println("IOException while reading input");
+        }
     }
 
     public static void print(TreeNode root) {
@@ -23,10 +29,22 @@ public class TreeGraph {
     }
 
     private static void print(TreeNode root, String prefix, boolean isTail) {
-        System.out.println(prefix + (isTail ? "└── " : "├──" + String.valueOf(root.getWeight()) + "── " ) + root.getName());
+        String tail = "└── ";
+        String spaces = "      ";
+        if (!isTail) {
+            String weight = String.valueOf(root.getWeight());
+            tail = "├──" + weight + "── ";
+            for (int i = 0; i < weight.length(); i++) {
+                spaces += " ";
+            }
+        }
+        System.out.println(prefix + tail + root.getName());
         List<TreeNode> children = root.getChildren();
-        if (children == null) return;
-        children.forEach(child -> print(child ,prefix + (isTail ? "    " : "│         "), false));
+        if (children == null) {
+            return;
+        }
+        String whitespaces = spaces;
+        children.forEach(child -> print(child, prefix + (isTail ? "    " : "│" + whitespaces), false));
     }
-    
+
 }
