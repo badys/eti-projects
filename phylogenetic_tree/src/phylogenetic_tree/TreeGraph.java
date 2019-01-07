f074dca093f1e50eddab28518f1934863e94f89d
badys <piotr.badysiak@gmail.com>
2019-01-07 23:22:04 +0100

    better vertical lines on print

--- phylogenetic_tree/src/phylogenetic_tree/TreeGraph.java
+++ phylogenetic_tree/src/phylogenetic_tree/TreeGraph.java
@@ -9,26 +9,26 @@ import java.util.List;
  */
 public class TreeGraph {
 
-    public static final String TAIL = "\u2514";
     public static final String HORIZONTAL = "\u2500\u2500";
     public static final String VERTICALWITHLINE = "\u251C";
     public static final String VERTICAL = "\u2502";
 
     public static void print(TreeNode root) {
         System.out.println("");
-        root.getChildren().forEach(tree -> print(tree, ""));
+        for (int i = 0; i < root.getChildren().size(); i++) {
+            print(root.getChildren().get(i), "", i, root.getChildren().size());
+        }
     }
 
-    private static void print(TreeNode root, String prefix) {
+    private static void print(TreeNode root, String prefix, int i, int size) {
         String tail = VERTICALWITHLINE + HORIZONTAL + HORIZONTAL + " ";
         String spaces = "    ";
         System.out.println(prefix + tail + root.getName());
         List<TreeNode> children = root.getChildren();
-        if (children == null) {
-            return;
-        }
         String whitespaces = spaces;
-        children.forEach(child -> print(child, prefix + VERTICAL + whitespaces));
+        for (int j = 0; j < children.size(); j++) {
+            print(children.get(j), prefix + (i < size-1 ? VERTICAL : "   ") + whitespaces, j, children.size());
+        }
     }
 
     public static void printTreeListInRawFormat(List<TreeNode> treeList) {
